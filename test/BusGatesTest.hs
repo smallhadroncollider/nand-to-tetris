ee27e8ebf626b0b0e66e6945a080a4b0f759ae47
Mark Wales <mark@smallhadroncollider.com>
2021-02-24 10:16:51 +0000

    refactor: update tests to use numbers

--- test/BusGatesTest.hs
+++ test/BusGatesTest.hs
@@ -5,8 +5,15 @@ module BusGatesTest
 import Test.Tasty
 import Test.Tasty.HUnit
 
-import Util (bit, bus8, bus16)
+import Util
 import Bus.Gates
+
+-- number reference
+-- ================
+-- 255: 11111111
+-- 32768: 1000000000000000
+-- 32769: 1000000000000001
+-- 65535: 1111111111111111
 
 -- tests
 test_bus_gates :: TestTree
@@ -15,149 +22,149 @@ test_bus_gates =
         "BusGates"
         [ testGroup "Bus16"
             [ testGroup "not16"
-                [ testCase "all 0s" (assertEqual "all 1s" (bus16 "1111111111111111") (not16 (bus16 "0000000000000000")))
+                [ testCase "all 0s" (assertEqual "all 1s" (bus16 65535) (not16 (bus16 0)))
                 ]
             , testGroup "and16"
-                [ testCase "all 0s" (assertEqual "all 0s" (bus16 "0000000000000000") (bus16 "0000000000000000" `and16` bus16 "0000000000000000"))
-                , testCase "some 1s" (assertEqual "all 0s" (bus16 "0000000000000000") (bus16 "1000000000000001" `and16` bus16 "0000000000000000"))
-                , testCase "some 1s" (assertEqual "some 1s" (bus16 "1000000000000001") (bus16 "1000000000000001" `and16` bus16 "1000000000000001"))
-                , testCase "all 1s" (assertEqual "all 1s" (bus16 "1111111111111111") (bus16 "1111111111111111" `and16` bus16 "1111111111111111"))
+                [ testCase "all 0s" (assertEqual "all 0s" (bus16 0) (bus16 0 `and16` bus16 0))
+                , testCase "some 1s" (assertEqual "all 0s" (bus16 0) (bus16 32769 `and16` bus16 0))
+                , testCase "some 1s" (assertEqual "some 1s" (bus16 32769) (bus16 32769  `and16` bus16 32769))
+                , testCase "all 1s" (assertEqual "all 1s" (bus16 65535) (bus16 65535 `and16` bus16 65535))
                 ]
             , testGroup "or16"
-                [ testCase "all 0s" (assertEqual "all 0s" (bus16 "0000000000000000") (bus16 "0000000000000000" `or16` bus16 "0000000000000000"))
-                , testCase "some 1s" (assertEqual "some 1s" (bus16 "1000000000000001") (bus16 "1000000000000000" `or16` bus16 "0000000000000001"))
-                , testCase "all 1s" (assertEqual "all 1s" (bus16 "1111111111111111") (bus16 "1111111111111111" `or16` bus16 "1111111111111111"))
+                [ testCase "all 0s" (assertEqual "all 0s" (bus16 0) (bus16 0 `or16` bus16 0))
+                , testCase "some 1s" (assertEqual "some 1s" (bus16 32769) (bus16 32768 `or16` bus16 1))
+                , testCase "all 1s" (assertEqual "all 1s" (bus16 65535) (bus16 65535 `or16` bus16 65535))
                 ]
             , testGroup "mux16"
-                [ testCase "all 0s" (assertEqual "all 0s" (bus16 "0000000000000000") (mux16 (bus16 "0000000000000000") (bus16 "1111111111111111") (bit "0")))
-                , testCase "all 0s" (assertEqual "all 0s" (bus16 "1111111111111111") (mux16 (bus16 "0000000000000000") (bus16 "1111111111111111") (bit "1")))
+                [ testCase "all 0s" (assertEqual "all 0s" (bus16 0) (mux16 (bus16 0) (bus16 65535) (bit "0")))
+                , testCase "all 0s" (assertEqual "all 0s" (bus16 65535) (mux16 (bus16 0) (bus16 65535) (bit "1")))
                 ]
             ]
         , testGroup "Bus8"
             [ testGroup "or8Way"
-                [ testCase "all 0s" (assertEqual "0" (bit "0") (or8Way (bus8 "00000000")))
-                , testCase "some 1s" (assertEqual "1" (bit "1") (or8Way (bus8 "00001000")))
-                , testCase "some 1s" (assertEqual "1" (bit "1") (or8Way (bus8 "00000001")))
-                , testCase "all 1s" (assertEqual "1" (bit "1") (or8Way (bus8 "11111111")))
+                [ testCase "all 0s" (assertEqual "0" (bit "0") (or8Way (bus8 0)))
+                , testCase "some 1s" (assertEqual "1" (bit "1") (or8Way (bus8 8)))
+                , testCase "some 1s" (assertEqual "1" (bit "1") (or8Way (bus8 1)))
+                , testCase "all 1s" (assertEqual "1" (bit "1") (or8Way (bus8 255)))
                 ]
             ]
         , testGroup "Multiplexors"
             [ testGroup "mux4Way16"
-                [ testCase "first" (assertEqual "first input" (bus16 "0000000000000000") (mux4Way16 (bus16 "0000000000000000") (bus16 "0000000000000001") (bus16 "0000000000000010") (bus16 "0000000000000011") (bit "0") (bit "0")))
-                , testCase "second" (assertEqual "second input" (bus16 "0000000000000001") (mux4Way16 (bus16 "0000000000000000") (bus16 "0000000000000001") (bus16 "0000000000000010") (bus16 "0000000000000011") (bit "0") (bit "1")))
-                , testCase "third" (assertEqual "third input" (bus16 "0000000000000010") (mux4Way16 (bus16 "0000000000000000") (bus16 "0000000000000001") (bus16 "0000000000000010") (bus16 "0000000000000011") (bit "1") (bit "0")))
-                , testCase "fourth" (assertEqual "fourth input" (bus16 "0000000000000011") (mux4Way16 (bus16 "0000000000000000") (bus16 "0000000000000001") (bus16 "0000000000000010") (bus16 "0000000000000011") (bit "1") (bit "1")))
+                [ testCase "first" (assertEqual "first input" (bus16 0) (mux4Way16 (bus16 0) (bus16 1) (bus16 2) (bus16 3) (bit "0") (bit "0")))
+                , testCase "second" (assertEqual "second input" (bus16 1) (mux4Way16 (bus16 0) (bus16 1) (bus16 2) (bus16 3) (bit "0") (bit "1")))
+                , testCase "third" (assertEqual "third input" (bus16 2) (mux4Way16 (bus16 0) (bus16 1) (bus16 2) (bus16 3) (bit "1") (bit "0")))
+                , testCase "fourth" (assertEqual "fourth input" (bus16 3) (mux4Way16 (bus16 0) (bus16 1) (bus16 2) (bus16 3) (bit "1") (bit "1")))
                 ]
             , testGroup "mux8Way16"
                 [ testCase "first"
-                    (assertEqual "first input" (bus16 "0000000000000000")
+                    (assertEqual "first input" (bus16 0)
                         (mux8Way16
-                            (bus16 "0000000000000000")
-                            (bus16 "0000000000000001")
-                            (bus16 "0000000000000010")
-                            (bus16 "0000000000000011")
-                            (bus16 "0000000000000100")
-                            (bus16 "0000000000000101")
-                            (bus16 "0000000000000110")
-                            (bus16 "0000000000000111")
+                            (bus16 0)
+                            (bus16 1)
+                            (bus16 2)
+                            (bus16 3)
+                            (bus16 4)
+                            (bus16 5)
+                            (bus16 6)
+                            (bus16 7)
                             (bit "0") (bit "0") (bit "0")
                         )
                     )
                 , testCase "second"
-                    (assertEqual "second input" (bus16 "0000000000000001")
+                    (assertEqual "second input" (bus16 1)
                         (mux8Way16
-                            (bus16 "0000000000000000")
-                            (bus16 "0000000000000001")
-                            (bus16 "0000000000000010")
-                            (bus16 "0000000000000011")
-                            (bus16 "0000000000000100")
-                            (bus16 "0000000000000101")
-                            (bus16 "0000000000000110")
-                            (bus16 "0000000000000111")
+                            (bus16 0)
+                            (bus16 1)
+                            (bus16 2)
+                            (bus16 3)
+                            (bus16 4)
+                            (bus16 5)
+                            (bus16 6)
+                            (bus16 7)
                             (bit "0") (bit "0") (bit "1")
                         )
                     )
                 , testCase "third"
-                    (assertEqual "third input" (bus16 "0000000000000010")
+                    (assertEqual "third input" (bus16 2)
                         (mux8Way16
-                            (bus16 "0000000000000000")
-                            (bus16 "0000000000000001")
-                            (bus16 "0000000000000010")
-                            (bus16 "0000000000000011")
-                            (bus16 "0000000000000100")
-                            (bus16 "0000000000000101")
-                            (bus16 "0000000000000110")
-                            (bus16 "0000000000000111")
+                            (bus16 0)
+                            (bus16 1)
+                            (bus16 2)
+                            (bus16 3)
+                            (bus16 4)
+                            (bus16 5)
+                            (bus16 6)
+                            (bus16 7)
                             (bit "0") (bit "1") (bit "0")
                         )
                     )
                 , testCase "fourth"
-                    (assertEqual "fourth input" (bus16 "0000000000000011")
+                    (assertEqual "fourth input" (bus16 3)
                         (mux8Way16
-                            (bus16 "0000000000000000")
-                            (bus16 "0000000000000001")
-                            (bus16 "0000000000000010")
-                            (bus16 "0000000000000011")
-                            (bus16 "0000000000000100")
-                            (bus16 "0000000000000101")
-                            (bus16 "0000000000000110")
-                            (bus16 "0000000000000111")
+                            (bus16 0)
+                            (bus16 1)
+                            (bus16 2)
+                            (bus16 3)
+                            (bus16 4)
+                            (bus16 5)
+                            (bus16 6)
+                            (bus16 7)
                             (bit "0") (bit "1") (bit "1")
                         )
                     )
                 , testCase "fifth"
-                    (assertEqual "fifth input" (bus16 "0000000000000100")
+                    (assertEqual "fifth input" (bus16 4)
                         (mux8Way16
-                            (bus16 "0000000000000000")
-                            (bus16 "0000000000000001")
-                            (bus16 "0000000000000010")
-                            (bus16 "0000000000000011")
-                            (bus16 "0000000000000100")
-                            (bus16 "0000000000000101")
-                            (bus16 "0000000000000110")
-                            (bus16 "0000000000000111")
+                            (bus16 0)
+                            (bus16 1)
+                            (bus16 2)
+                            (bus16 3)
+                            (bus16 4)
+                            (bus16 5)
+                            (bus16 6)
+                            (bus16 7)
                             (bit "1") (bit "0") (bit "0")
                         )
                     )
                 , testCase "sixth"
-                    (assertEqual "sixth input" (bus16 "0000000000000101")
+                    (assertEqual "sixth input" (bus16 5)
                         (mux8Way16
-                            (bus16 "0000000000000000")
-                            (bus16 "0000000000000001")
-                            (bus16 "0000000000000010")
-                            (bus16 "0000000000000011")
-                            (bus16 "0000000000000100")
-                            (bus16 "0000000000000101")
-                            (bus16 "0000000000000110")
-                            (bus16 "0000000000000111")
+                            (bus16 0)
+                            (bus16 1)
+                            (bus16 2)
+                            (bus16 3)
+                            (bus16 4)
+                            (bus16 5)
+                            (bus16 6)
+                            (bus16 7)
                             (bit "1") (bit "0") (bit "1")
                         )
                     )
                 , testCase "seventh"
-                    (assertEqual "seventh input" (bus16 "0000000000000110")
+                    (assertEqual "seventh input" (bus16 6)
                         (mux8Way16
-                            (bus16 "0000000000000000")
-                            (bus16 "0000000000000001")
-                            (bus16 "0000000000000010")
-                            (bus16 "0000000000000011")
-                            (bus16 "0000000000000100")
-                            (bus16 "0000000000000101")
-                            (bus16 "0000000000000110")
-                            (bus16 "0000000000000111")
+                            (bus16 0)
+                            (bus16 1)
+                            (bus16 2)
+                            (bus16 3)
+                            (bus16 4)
+                            (bus16 5)
+                            (bus16 6)
+                            (bus16 7)
                             (bit "1") (bit "1") (bit "0")
                         )
                     )
                 , testCase "eigth"
-                    (assertEqual "eigth input" (bus16 "0000000000000111")
+                    (assertEqual "eigth input" (bus16 7)
                         (mux8Way16
-                            (bus16 "0000000000000000")
-                            (bus16 "0000000000000001")
-                            (bus16 "0000000000000010")
-                            (bus16 "0000000000000011")
-                            (bus16 "0000000000000100")
-                            (bus16 "0000000000000101")
-                            (bus16 "0000000000000110")
-                            (bus16 "0000000000000111")
+                            (bus16 0)
+                            (bus16 1)
+                            (bus16 2)
+                            (bus16 3)
+                            (bus16 4)
+                            (bus16 5)
+                            (bus16 6)
+                            (bus16 7)
                             (bit "1") (bit "1") (bit "1")
                         )
                     )
--- test/Util.hs
+++ test/Util.hs
@@ -1,10 +1,15 @@
 {-# OPTIONS_GHC -Wno-orphans -Wno-incomplete-patterns #-}
-module Util where
+module Util
+    ( bit
+    , bus8
+    , bus16
+    ) where
 
-import Prelude (Show, Eq, Bool (True, False), show, (==), (<$>), (&&), concat)
+import Prelude
+import Numeric (showIntAtBase)
+import Data.Char (intToDigit)
 
-import Data.List ((!!))
-import Data.Text (Text, chunksOf)
+import Data.Text (Text, chunksOf, pack, justifyRight)
 
 import Bit.Data (Binary (One, Zero))
 import Bus.Data (Bus16 (Bus16), Bus8 (Bus8))
@@ -25,6 +30,14 @@ bit "0" = Zero
 bit "1" = One
 
 
+-- buses
+toBinary :: Int -> Text
+toBinary n = pack $ showIntAtBase 2 intToDigit n ""
+
+toBinaryText :: Int -> Int -> Text
+toBinaryText n len = justifyRight len '0' (toBinary n)
+
+
 -- bus8
 instance Eq Bus8 where
     (Bus8 a0 a1 a2 a3 a4 a5 a6 a7) == (Bus8 b0 b1 b2 b3 b4 b5 b6 b7) = (a0 == b0) && (a1 == b1) && (a2 == b2) && (a3 == b3) && (a4 == b4) && (a5 == b5) && (a6 == b6) && (a7 == b7)
@@ -32,9 +45,12 @@ instance Eq Bus8 where
 instance Show Bus8 where
     show (Bus8 i0 i1 i2 i3 i4 i5 i6 i7) = concat (show <$> [i0, i1, i2, i3, i4, i5, i6, i7])
 
-bus8 :: Text -> Bus8
-bus8 values = Bus8 (ls !! 0) (ls !! 1) (ls !! 2) (ls !! 3) (ls !! 4) (ls !! 5) (ls !! 6) (ls !! 7)
+bus8FromText :: Text -> Bus8
+bus8FromText values = Bus8 (ls !! 0) (ls !! 1) (ls !! 2) (ls !! 3) (ls !! 4) (ls !! 5) (ls !! 6) (ls !! 7)
     where ls = bit <$> chunksOf 1 values
+
+bus8 :: Int -> Bus8
+bus8 n = bus8FromText $ toBinaryText n 8
 
 
 -- bus16
@@ -44,6 +60,9 @@ instance Eq Bus16 where
 instance Show Bus16 where
     show (Bus16 i0 i1 i2 i3 i4 i5 i6 i7 i8 i9 i10 i11 i12 i13 i14 i15) = concat (show <$> [i0, i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14, i15])
 
-bus16 :: Text -> Bus16
-bus16 values = Bus16 (ls !! 0) (ls !! 1) (ls !! 2) (ls !! 3) (ls !! 4) (ls !! 5) (ls !! 6) (ls !! 7) (ls !! 8) (ls !! 9) (ls !! 10) (ls !! 11) (ls !! 12) (ls !! 13) (ls !! 14) (ls !! 15)
+bus16FromText :: Text -> Bus16
+bus16FromText values = Bus16 (ls !! 0) (ls !! 1) (ls !! 2) (ls !! 3) (ls !! 4) (ls !! 5) (ls !! 6) (ls !! 7) (ls !! 8) (ls !! 9) (ls !! 10) (ls !! 11) (ls !! 12) (ls !! 13) (ls !! 14) (ls !! 15)
     where ls = bit <$> chunksOf 1 values
+
+bus16 :: Int -> Bus16
+bus16 n = bus16FromText $ toBinaryText n 16
